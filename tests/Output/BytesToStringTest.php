7de727ef7f6e911e0d8f1dd3d0c52d28062b49cf
Florian Berberich <php.florianberberich@outlook.com>
2019-03-27 13:58:09 +0100

    test(input): implemented tests for output classes

---
+++ tests/Output/BytesToStringTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+namespace berbeflo\Brainfuck\Test\Output;
+
+use berbeflo\Brainfuck\Output\BytesToString;
+use PHPUnit\Framework\TestCase;
+
+class BytesToStringTest extends TestCase
+{
+    public function testWriteChars()
+    {
+        $bytesObject = new BytesToString();
+        $bytesObject->writeChar(\ord('a'));
+        $bytesObject->writeChar(\ord('b'));
+        $bytesObject->writeChar(\ord('c'));
+        $this->assertSame('abc', $bytesObject->getResult());
+    }
+
+    public function testReset()
+    {
+        $bytesObject = new BytesToString();
+        $i = 3;
+
+        while ($i-- > 0) {
+            $bytesObject->writeChar(\ord('a'));
+            $bytesObject->writeChar(\ord('b'));
+            $this->assertSame('ab', $bytesObject->getResult());
+            $this->assertSame('ab', $bytesObject->getResult());
+            $bytesObject->reset();
+        }
+    }
+
+    public function testEmptyResult()
+    {
+        $bytesObject = new BytesToString();
+        $this->assertSame('', $bytesObject->getResult());
+    }
+}
---
+++ tests/Output/NumbersToArrayTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+namespace berbeflo\Brainfuck\Test\Output;
+
+use berbeflo\Brainfuck\Output\NumbersToArray;
+use PHPUnit\Framework\TestCase;
+
+class NumbersToArrayTest extends TestCase
+{
+    public function testWriteNumbers()
+    {
+        $numbersObject = new NumbersToArray();
+        $numbersObject->writeChar(4);
+        $numbersObject->writeChar(8);
+        $this->assertSame([4, 8], $numbersObject->getResult());
+    }
+
+    public function testReset()
+    {
+        $numbersObject = new NumbersToArray();
+        $i = 3;
+
+        while ($i-- > 0) {
+            $numbersObject->writeChar(2);
+            $numbersObject->writeChar(4);
+            $this->assertSame([2, 4], $numbersObject->getResult());
+            $this->assertSame([2, 4], $numbersObject->getResult());
+            $numbersObject->reset();
+        }
+    }
+
+    public function testEmptyResult()
+    {
+        $numbersObject = new NumbersToArray();
+        $this->assertSame([], $numbersObject->getResult());
+    }
+}
