c1e4420005c2c4df06a17db2becf0ba09d00ab2a
Florian Berberich <php.florianberberich@outlook.com>
2019-03-27 14:21:49 +0100

    test(config): added a test case for the config class. skipped setters

---
+++ tests/ConfigTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+use berbeflo\Brainfuck\Config;
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
+
+class ConfigTest extends TestCase
+{
+    public function testDefaultInput()
+    {
+        $config = new Config();
+        $input = $config->getInputObject();
+        $this->expectException(RuntimeException::class, $input->getNextChar());
+    }
+
+    public function testDefaultOutput()
+    {
+        $config = new Config();
+        $output = $config->getOutputObject();
+        $this->expectException(RuntimeException::class, $output->writeChar(0));
+    }
+
+    public function testDefaultValues()
+    {
+        $config = new Config();
+        $this->assertSame(0, $config->getMinRegisterValue());
+        $this->assertSame(255, $config->getMaxRegisterValue());
+        $this->assertSame(0, $config->getMinPointerValue());
+        $this->assertSame(255, $config->getMaxPointerValue());
+        $this->assertFalse($config->wrapOnPointerOverflow());
+        $this->assertFalse($config->wrapOnRegisterOverflow());
+    }
+}
