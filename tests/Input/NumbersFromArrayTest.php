102d691c558c6716b3d053dc2f1fbbb61597a5b5
Florian Berberich <php.florianberberich@outlook.com>
2019-03-27 13:07:38 +0100

    test(input): wrote test for NumbersFromArray

---
+++ tests/Input/NumbersFromArrayTest.php
@@ -0,0 +1,29 @@
+<?php
+namespace berbeflo\Brainfuck\Test\Input;
+use berbeflo\Brainfuck\Input\NumbersFromArray;
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
+class NumbersFromArrayTest extends TestCase {
+    public function testGetNumbers() {
+        $numbersObject = new NumbersFromArray([2, 4, 8]);
+        $this->assertSame(2, $numbersObject->getNextChar());
+        $this->assertSame(4, $numbersObject->getNextChar());
+        $this->assertSame(8, $numbersObject->getNextChar());
+    }
+
+    public function testException() {
+        $numbersObject = new NumbersFromArray([]);
+        $this->expectException(RuntimeException::cass);
+        $numbersObject->getNextChar();
+    }
+
+    public function testReset() {
+        $numbersObject = new NumbersFromArray([2, 4]);
+        $i = 3;
+        while($i-- > 0) {
+            $this->assertSame(2, $numbersObject->getNextChar());
+            $this->assertSame(4, $numbersObject->getNextChar());
+            $numbersObject->reset();
+        }
+    }
+}
